7d208f638bdbb8d500b8ffcfb4a5ac1076010f70
samthecoder <49662629+SamTheCoder777@users.noreply.github.com>
2020-07-04 16:22:44 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -87,23 +87,15 @@ client.on("message", async message => {
   }
   
   if (message.content.startsWith(prefix + "tempmute")){
-    exports.run = (client, message, [mention, minutes, ...reason]) => {
-      // You need to parse those arguments, I'll leave that to you.
-
-      // This is the role you want to assign to the user
       let mutedRole = message.guild.roles.cache.find(role => role.name == "Muted");
       // This is the member you want to mute
       let member = message.mentions.members.first();
-
-      // Mute the user
-      member.roles.add(mutedRole, `Muted by ${message.author.tag} for ${minutes} minutes. Reason: ${reason}`);
-
-      // Unmute them after x minutes
-      setTimeout(() => {
-        member.roles.remove(mutedRole, `Temporary mute expired.`);
-      }, minutes * 60000); // time in ms
-      };
-    }
+      let sec = args.slice(1).join(" ");
+      if (!sec){
+        return message.reply('Please include the seconds');
+      }
+      member.addRole(mutedRole);
+      setTimeout(() => {member.removeRole(muteRole);}, sec * 1000);
  }
   
   if (message.content.startsWith(prefix + "unmute")) {
