4a1e265d7ad38bda8b54c26b6f36199b4c8899be
samthecoder <49662629+SamTheCoder777@users.noreply.github.com>
2020-07-07 14:45:25 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on("message", async message => {
 	if (!amount) return message.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the `amount` parameter is given
 	if (isNaN(amount)) return message.reply('The amount needs to be a number!'); // Checks if the `amount` parameter is a number. If not, the command throws an error
 
-	if (amount > 200) return message.reply('You can`t delete more than 200 messages at once!'); // Checks if the `amount` integer is bigger than 100
+	if (amount > 100) return message.reply('You can`t delete more than 100 messages at once!'); // Checks if the `amount` integer is bigger than 100
 	if (amount < 1) return message.reply('You have to delete at least 1 message!'); // Checks if the `amount` integer is smaller than 1
 
 	await message.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
